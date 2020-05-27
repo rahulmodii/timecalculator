29d8f4360e5ac093f47b022096d878a11afc2bf7
rmodi234 <rahul.modi@neologicx.com>
2020-05-27 22:11:39 +0530

    time cal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card from 'react-bootstrap/Card'
+import TimePicker from 'react-time-picker';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    time: '10:00',
+    fromtimer:'10:00'
+  }
+
+  onChange = time => {
+    // console.log(time)
+    // const starttime= moment("2020-05-27 9:30")
+    // console.log(starttime)
+    // const a = moment([21,30,00], "HH:mm:ss")
+    // const b = moment([09,30,00], "HH:mm:ss")
+    // a.diff(b, 'hours') 
+    this.setState({fromtimer:time})
+    const startTime = '09:30'
+    const endTime = time
+    const start = moment(startTime, "HH:mm");
+    const end = moment(endTime, "HH:mm");
+     const hrs= moment.duration(moment(end, "HH:mm").diff(moment(start, "HH:mm")))
+    console.log(hrs.hours()-1,hrs.minutes())
+    const times= `${hrs.hours()-1}:${hrs.minutes()}`
+    console.log(times)
+    this.setState({time:times})
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <TimePicker
+          onChange={this.onChange}
+          value={this.state.fromtimer}
+        />
+        <Card>
+          <Card.Body style={{fontSize:200}}>{this.state.time}</Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default App;
+
