3e58b703e3b4cad1ec6a2fac7e53d6bf11d80d75
rmodi234 <rahul.modi@neologicx.com>
2020-05-27 22:32:13 +0530

    font

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
           value={this.state.fromtimer}
         />
         <Card>
-          <Card.Body style={{fontSize:200}}>{this.state.time}Today you dumbfuck</Card.Body>
+          <Card.Body style={{fontSize:100}}>{this.state.time}Today you dumbfuck</Card.Body>
         </Card>
       </div>
     );
